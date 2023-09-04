7203bf7a294d635e59dd92c6104befe6f9f5d6e9
John Doe <johndoe@example.com>
2023-09-04 21:37:18 +0800

    test: Add more testcases for bing translator

--- internal/translator/bing/translate_test.go
+++ internal/translator/bing/translate_test.go
@@ -43,3 +43,35 @@ func TestTranslateText_MultipleLines(t *testing.T) {
 	assert.Contains(t, res.TranslatedText, "我不希望地球不惜一切代价成为一个死亡的星球。")
 	assert.Contains(t, res.TranslatedText, "面向未来，我们的目标是与地球上的所有生物共存，我认为未来可能是人类真正的“黎明”")
 }
+
+func TestTranslateText_SourceUnsupported(t *testing.T) {
+	recorder, _ := recorder.New("fixtures/TestTranslateText_SourceUnsupported")
+	defer func() { _ = recorder.Stop() }()
+
+	conf := conf.ConfTranslator{}
+	client := translator.BuildClient(conf)
+	client.SetTransport(recorder)
+	pdata, _ := translator.Preflight(client, conf)
+	res := translator.MakeRequest(client,
+		"",
+		"ii", "zh-CN", conf, pdata,
+	)
+	assert.Equal(t, 1, res.Code)
+	assert.Contains(t, res.Error, "400")
+}
+
+func TestTranslateText_TargetUnsupported(t *testing.T) {
+	recorder, _ := recorder.New("fixtures/TestTranslateText_TargetUnsupported")
+	defer func() { _ = recorder.Stop() }()
+
+	conf := conf.ConfTranslator{}
+	client := translator.BuildClient(conf)
+	client.SetTransport(recorder)
+	pdata, _ := translator.Preflight(client, conf)
+	res := translator.MakeRequest(client,
+		"Quickly translate words and phrases between English and over 100 languages.",
+		"", "ii", conf, pdata,
+	)
+	assert.Equal(t, 1, res.Code)
+	assert.Contains(t, res.Error, "400")
+}
