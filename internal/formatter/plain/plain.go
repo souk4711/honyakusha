b2d1f3ce0d5eccafd6432cac7f02e43839919bfd
John Doe <johndoe@example.com>
2023-09-04 22:03:27 +0800

    fix: Fix plaintext formatter bug when outputting multiple lines

--- internal/formatter/plain/plain.go
+++ internal/formatter/plain/plain.go
@@ -1,6 +1,8 @@
 package plain
 
 import (
+	"strings"
+
 	"github.com/souk4711/honyakusha/internal/res"
 )
 
@@ -10,13 +12,25 @@ func Format(res res.Res) string {
 	}
 
 	r := ""
-	for _, translator := range res.Translators {
+	for i, translator := range res.Translators {
+		if i != 0 {
+			r = r + "\n"
+		}
+
 		r = r + translator.Translator.Name + ":\n"
 		if translator.Code != 0 {
-			r = r + "\n    " + translator.Error + "\n\n"
+			r = r + indent(translator.Error) + "\n"
 		} else {
-			r = r + "\n    " + translator.TranslatedText + "\n\n"
+			r = r + indent(translator.TranslatedText) + "\n"
 		}
 	}
 	return r
+}
+
+func indent(str string) string {
+	var arr []string
+	for _, ele := range strings.Split(strings.TrimSpace(str), "\n") {
+		arr = append(arr, "    "+ele)
+	}
+	return strings.Join(arr, "\n")
 }
