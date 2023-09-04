11f15b043f1218bec707381d45fe46de2a2e78e3
John Doe <johndoe@example.com>
2023-09-04 10:57:13 +0800

    feat: Add format flag to subcommand trans

--- internal/cmd/trans.go
+++ internal/cmd/trans.go
@@ -11,15 +11,18 @@ import (
 )
 
 func newTransCommand() *cobra.Command {
+	var Format string
+
 	var transCommand = &cobra.Command{
 		Use:   "trans TEXT",
 		Short: "Translate text",
 		Run: func(cmd *cobra.Command, args []string) {
 			c := conf.Load()
 			res := trans.Translate(strings.Join(args, " "), c)
-			fmt.Print(trans.Format(res, "plain"))
+			fmt.Print(trans.Format(res, Format))
 		},
 	}
 
+	transCommand.Flags().StringVarP(&Format, "format", "f", "plain", "Choose an output formatter")
 	return transCommand
 }
